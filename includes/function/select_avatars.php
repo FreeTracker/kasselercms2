34c4673d44ea41dbc8b3aa9e98d79bc6255d968f
HEPOH <hepoh@live.ru>
2015-05-16 20:15:58 +0600

    select_avatars
    переменная $classs для выпадающего списка - групп аватаров

--- includes/function/select_avatars.php
+++ includes/function/select_avatars.php
@@ -5,10 +5,10 @@ if(!defined('FUNC_FILE')) die('Access is limited');
 * 
 * @return
 */
-function select_avatars(){
+function select_avatars($class="select2 chzn-search-hide"){
 global $userconf;
     if(hook_check(__FUNCTION__)) return hook();
-    $select = "\n<select id='cat' class='select2 chzn-search-hide' name='avatar'>\n";
+    $select = "\n<select id='cat' class='{$class}' name='avatar'>\n";
     $avatars = opendir($userconf['directory_avatar']);
     while(($file = readdir($avatars))) if(is_dir($userconf['directory_avatar'].$file) AND $file!='.' AND $file!='..' AND $file!='admin' AND $file!='.svn') $select .= "<option value='{$file}'>$file</option>\n";
     closedir($avatars);
